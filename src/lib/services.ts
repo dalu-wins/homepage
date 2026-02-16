31c7657d547de6b37db1b46c311541f1023a3fa3
Dalu Wins <dalu-wins@users.noreply.github.com>
2026-02-16 04:06:33 +0100

    feat: added navbar and darkmode

---
+++ src/lib/services.ts
@@ -0,0 +1,14 @@
+export const services = [
+    {
+        name: 'Service',
+        description: 'First Service.',
+        url: 'https://example.com',
+        icon: '🚆'
+    },
+    {
+        name: 'Service',
+        description: 'Second Service.',
+        url: 'https://example.com',
+        icon: '🚀'
+    }
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+ <script lang="ts">
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode } from "flowbite-svelte";
+</script>
+
+<Navbar>
+
+    <NavBrand href="/">
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Daniil Wins</span>
+    </NavBrand>
+
+    <div class="flex items-center gap-2 md:order-2">
+        <NavHamburger />
+
+        <NavUl class="md:flex-row md:gap-6 px-2">
+            <NavLi href="/">Profile</NavLi>
+            <NavLi href="/projects">Projects</NavLi>
+            <NavLi href="/curriculum">Curriculum</NavLi>
+            <NavLi href="/contact">Contact</NavLi>
+        </NavUl>
+
+        <DarkMode />
+    </div>
+
+</Navbar>
+
