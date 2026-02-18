ec4aee502ba0968fed29f92e9f3c597593ae07dc
Daniil Wins <dalu-wins@users.noreply.github.com>
2026-02-18 11:30:12 +0100

    fix: add meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 <svelte:head>
     <title>homepage</title>
     <link rel="icon" href={profile.avatarUrl} />
+    <meta 
+        name="description" 
+        content="Explore the portfolio of dalu-wins. Featuring mobile and web development projects, a curriculum and contact information." 
+    />
 </svelte:head>
 
 {@render children()} 
